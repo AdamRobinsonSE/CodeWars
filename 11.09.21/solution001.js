596708449fc4adfdd5702f000cb7086ff686aea1
Adam Robinson <robinson.db@gmail.com>
2021-11-09 08:07:19 -0500

    added solution001.js

--- 11.09.21/solution001.js
+++ 11.09.21/solution001.js
@@ -13,7 +13,6 @@ function iceBrickVolume(radius, bottleLength, rimLength) {
     var brickLength = bottleLength - rimLength;
     var brickDepth = radius*2;
     var brickHeight = radius; 
-    
     return brickLength * brickDepth * brickHeight;
-    
 };
+
