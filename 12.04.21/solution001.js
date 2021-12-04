bc0b7f85e0a60ad9deeb5e0b6e80e7398c9b80c7
Adam Robinson <robinson.db@gmail.com>
2021-12-04 12:41:09 -0500

    added solution001.js

---
+++ 12.04.21/solution001.js
@@ -0,0 +1,21 @@
+// Create a method each_cons that accepts a list and a number n, and returns cascading subsets of the list of size n, like so:
+
+// each_cons([1,2,3,4], 2)
+//   #=> [[1,2], [2,3], [3,4]]
+
+// each_cons([1,2,3,4], 3)
+//   #=> [[1,2,3],[2,3,4]]
+  
+// As you can see, the lists are cascading; ie, they overlap, but never out of order.
+
+function eachCons(array, n) {
+	let chunk = []
+    for(let i = 0 ; i < array.length ; i++){
+        let newArr = array.slice(i, i + n)
+        chunk.push(newArr)
+    }
+    return chunk.filter(e => e.length === n)
+};
+
+console.log(eachCons([1,2,3,4],2))
+console.log(eachCons([1,2,3,4],3))
