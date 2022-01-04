2a702b5ec8938926646dce791b648650247fee67
Adam Robinson <robinson.db@gmail.com>
2022-01-04 13:09:07 -0500

    added solution001.js

---
+++ 01.04.22/solution001.js
@@ -0,0 +1,36 @@
+// Freestyle Sparring
+// Take turns remixing and refactoring others code through Kumite
+// CAREER
+// Opportunities
+// Find your next career challenge – powered by Qualified Jobs
+// COMMUNITY
+// Leaderboards
+// Achieve honor and move up the global leaderboards
+// Chat
+// Join our Discord server and chat with your fellow code warriors
+// Discussions
+// View our Github Discussions board to discuss general Codewars topics
+// ABOUT
+// Docs
+// Learn about all of the different aspects of Codewars
+// AdamRobinsonSE Avatar
+// 5 kyu
+// 714
+// 8 kyu
+// Formatting decimal places #0
+// 783789% of 1,6513,750 of 11,591coder4fun3 Issues Reported
+//  JavaScript
+// Node v10.x
+// VIM
+// EMACS
+// Instructions
+// Output
+// Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+// Example:    
+// 5.5589 is rounded 5.56   
+// 3.3424 is rounded 3.34
+
+function twoDecimalPlaces(n) {
+    return +n.toFixed(2)    
+};
