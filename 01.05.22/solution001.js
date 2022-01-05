4a3bb0888ed90f608cfc2fbf092875dea69df91c
Adam Robinson <robinson.db@gmail.com>
2022-01-05 13:12:29 -0500

    added solution001.js

---
+++ 01.05.22/solution001.js
@@ -0,0 +1,9 @@
+// This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+
+// Whats given
+// const solve = (x, y) => x // y
+
+//Fixed
+const solve = function (x,y){
+    return x / y
+}
