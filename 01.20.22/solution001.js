f7c7fad85c558f7cc03b5c39ff290cf51fff6bd0
Adam Robinson <robinson.db@gmail.com>
2022-01-20 05:44:23 -0500

    added solution001.js

---
+++ 01.20.22/solution001.js
@@ -0,0 +1,16 @@
+// Variable assignment
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+//whats given
+
+// var a == "dev"
+// var b == "Lab"
+
+// var name == a + b
+
+//fixed
+
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
