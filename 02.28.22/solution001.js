132c444605d0be211749fdc80d1063d71e6d2f3b
Adam Robinson <robinson.db@gmail.com>
2022-02-28 05:59:16 -0500

    added solution001.js

---
+++ 02.28.22/solution001.js
@@ -0,0 +1,18 @@
+// Debugging sayHello function
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output:
+
+// Hello, Mr. Spock
+
+//PREP
+//P: given a string name
+//R: return 'Hello name'
+//E:
+
+function sayHello(name) {
+    return `Hello, ${name}`
+  }
+
+sayHello('Mr. Spock')
+sayHello('Captain Kirk')
