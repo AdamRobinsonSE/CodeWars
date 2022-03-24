965e0b7d69c1cf7c3fae03d5d75046d44a7c6e38
Adam Robinson <robinson.db@gmail.com>
2022-03-23 23:08:15 -0400

    added solution001.js

---
+++ 03.23.22/solution001.js
@@ -0,0 +1,40 @@
+// Task
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+// Mind the input validation.
+
+// Example
+// { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+// Input validation
+// If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+
+//PREP
+//P: Given an array of numbers, add them all together minus the lowest and highest value, if its empty, or only has 1 value, return 0
+//R: return a number
+
+function sumArray(array) {
+    if (array == null) {
+      return 0;
+    } else if (array.length < 2) {
+      return 0;
+    } else {
+      array = array.sort(function(a,b) {return a - b;});
+      var total = 0;
+      for (var i = 1; i < array.length - 1; i++) {
+        total += array[i];
+      }
+      return total;
+    }
+  }
+
+
+//E:
+
+sumArray([])
+sumArray([3])
+sumArray([3,5])
+sumArray([6,2,1,8,10])
+sumArray([0,1,6,10,10])
