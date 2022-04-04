720231ef5e7a0e70a89abc81527d05fdd39cf362
Adam Robinson <robinson.db@gmail.com>
2022-04-04 07:53:40 -0400

    added more

--- 4.4.22.js
+++ 4.4.22.js
@@ -11,3 +11,12 @@ function sumOfDifferences(arr){
     let newArr = arr.map((a,i) => a - arr[i + 1] || 0) // creates newArr that subtracts consecutive numbers
     return newArr.reduce((a,b) => a + b, 0) // returns the sum of the newArr
 }
+
+//Description:
+//Complete the solution so that it reverses all of the words within the string passed in.
+//Example:
+//"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+function reverseWrods(str){
+    return str.split(' ').reverse().join(' ') // first turns the str into an array, then reverses the array, then joins it all back together
+}
