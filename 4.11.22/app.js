ce31fb96baba790727ee2811de048aea396e0799
Adam Robinson <robinson.db@gmail.com>
2022-04-11 08:26:56 -0400

    added more

--- 4.11.22/app.js
+++ 4.11.22/app.js
@@ -57,3 +57,19 @@ function reverseNumberToArray(n){
     .split('')
     .reverse()
 }
+
+//Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+
+//E.g.
+//
+//arrayMadness([4, 5, 6], [1, 2, 3]); // returns true since 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
+
+function arrayMadness(a,b){
+    let arr1 = a.reduce((acc, c) => acc + (Math.pow(c,2)), 0)
+    let arr2 = b.reduce((acc,c) => acc + (Math.pow(c,3)), 0)
+    if (arr1 > arr2){
+        return true
+    }else {
+        return false
+    }
+}
