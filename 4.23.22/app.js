d57deb9ffcef2e30bf3e7d072573fa218fb459e8
Adam Robinson <robinson.db@gmail.com>
2022-04-25 13:21:07 -0400

    daily push

---
+++ 4.23.22/app.js
@@ -0,0 +1,11 @@
+//Create a function with two arguments that will return an array of the first(n) multiples of (x).
+//example
+//countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+//countBy(2,5) === [2,4,6,8,10]
+
+function multiples(n,x) {
+    let result = []
+    for (let i = 1 ; i < x ; i++){
+        result.push(n * i)
+    }
+}
