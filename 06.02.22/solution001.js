b96bdecd3ed430e588279a42c60707aee470502d
Adam Robinson <robinson.db@gmail.com>
2022-06-02 17:04:53 -0400

    test

--- 06.02.22/solution001.js
+++ 06.02.22/solution001.js
@@ -13,7 +13,5 @@
 // ENJOY CODING :)
 
 function sumOfMinimums(arr) {
-   return arr.reduce((acc,c) => acc + Math.min(...c),0)
-};
-
-console.log(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]))
+    return arr.reduce((p, c) => p + Math.min(...c), 0);
+  }
