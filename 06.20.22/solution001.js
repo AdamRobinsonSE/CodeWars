adb950d9a2b8932ec0355a469b1d88e04a4fc204
Adam Robinson <robinson.db@gmail.com>
2022-06-20 09:27:26 -0400

    test

--- 06.20.22/solution001.js
+++ 06.20.22/solution001.js
@@ -18,6 +18,6 @@ function solve(arr){
 };
 //E:
 
-console.log(solve(["abode"])) // [4,3,1]
+console.log(solve(["abode","ABc","xyzD"])) // [4,3,1]
 console.log(solve(["IAMDEFANDJKL","thedefgh","xyzDEFghijabc"])) // [6,5,7]
 console.log(solve(["encode","abc","xyzD","ABmD"])) // [1,3,1,3]
