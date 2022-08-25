e013e6952f759c68a0d5143f940206c46284f716
Adam Robinson <robinson.db@gmail.com>
2022-08-25 14:22:24 -0400

    fixed blank solution

--- 08.24.22/solution001.js
+++ 08.24.22/solution001.js
@@ -14,7 +14,18 @@
 // Please also try Simple remove duplicates
 
 function solve(a,b){
-
+    let result = ''
+    for(let i = 0 ; i < a.length ; i++){
+        if(!b.includes(a[i])){
+            result += a[i]
+        }
+    }
+    for(let i = 0 ; i < b.length ; i++){
+        if (!a.includes([b[i]])){
+            result += b[i]
+        }
+    }
+    return result
 };
 
 console.log(solve("xyab","xzca")) // "ybzc"
