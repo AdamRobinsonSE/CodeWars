4f538906718b961eeedefc6cb12588e39cf1fab3
Adam Robinson <robinson.db@gmail.com>
2022-09-26 08:10:35 -0400

    added solution001.js

---
+++ 09.26.22/solution001.js
@@ -0,0 +1,69 @@
+// Happy Holidays fellow Code Warriors!
+// Now, Dasher! Now, Dancer! Now, Prancer, and Vixen! On, Comet! On, Cupid! On, Donder and Blitzen! That's the order Santa wanted his reindeer...right? What do you mean he wants them in order by their last names!? Looks like we need your help Code Warrior!
+
+// Sort Santa's Reindeer
+// Write a function that accepts a sequence of Reindeer names, and returns a sequence with the Reindeer names sorted by their last names.
+
+// Notes:
+// It's guaranteed that each string is composed of two words
+// In case of two identical last names, keep the original order
+// Examples
+// For this input:
+
+//PREP
+//PARAMETERS: given an array of first and last names as strings
+
+//RETURN: Return the list sorted alphabetically by the last names
+
+//EXAMPLES
+
+// [
+//   "Dasher Tonoyan", 
+//   "Dancer Moore", 
+//   "Prancer Chua", 
+//   "Vixen Hall", 
+//   "Comet Karavani",        
+//   "Cupid Foroutan", 
+//   "Donder Jonker", 
+//   "Blitzen Claus"
+// ]
+// You should return this output:
+
+// [
+//   "Prancer Chua",
+//   "Blitzen Claus",
+//   "Cupid Foroutan", 
+//   "Vixen Hall", 
+//   "Donder Jonker", 
+//   "Comet Karavani",
+//   "Dancer Moore", 
+//   "Dasher Tonoyan",
+// ]
+
+//PSEUDO CODE
+
+function sortReindeer(reindeerNames) {
+    let sorted = reindeerNames.sort(function (a,b) {
+        var aName = a.split(' ')
+        var bName = b.split(' ')
+    
+        var aLastName = aName[aName.length-1];
+        var bLastName = bName[bName.length-1];
+    
+        if(aLastName < bLastName) return -1;
+        if(aLastName > bLastName) return 1;
+        return 0;
+      })
+      return sorted
+};
+
+console.log(sortReindeer([
+    "Dasher Tonoyan", 
+    "Dancer Moore", 
+    "Prancer Chua", 
+    "Vixen Hall", 
+    "Comet Karavani",        
+    "Cupid Foroutan", 
+    "Donder Jonker", 
+    "Blitzen Claus"
+  ]))
