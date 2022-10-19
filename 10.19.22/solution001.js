0daa329c3e9bc1be3e99b5487a76dc4c3d814bd1
Adam Robinson <robinson.db@gmail.com>
2022-10-19 10:41:58 -0400

    added solution001.js

---
+++ 10.19.22/solution001.js
@@ -0,0 +1,29 @@
+// In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
+
+// For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
+
+// More examples in the test cases.
+
+// Good luck!
+
+// If you like this Kata, please try:
+
+// Sum of prime-indexed elements
+
+// Sum of integer combinations
+
+// -----
+
+// given an array of integers
+
+// find the only two integers that do not repeat and add them together
+
+function repeats(arr){  
+    let filtered = arr.filter((e,i) => arr.indexOf(e) === arr.lastIndexOf(e)) 
+    return filtered.reduce((acc,c) => acc + c, 0)
+};
+
+// examples
+console.log(repeats([4,5,7,5,4,8])) // 15
+console.log(repeats([9, 10, 19, 13, 19, 13])) // 19
+console.log(repeats([5, 17, 18, 11, 13, 18, 11, 13])) // 22
