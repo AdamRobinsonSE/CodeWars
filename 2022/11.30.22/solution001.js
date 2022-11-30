19555065f45e3c486824d2efe52a5732641cb184
Adam Robinson <robinson.db@gmail.com>
2022-11-30 18:45:04 -0500

    added solution001.js

---
+++ 2022/11.30.22/solution001.js
@@ -0,0 +1,13 @@
+// Sort the given array of strings in alphabetical order, case insensitive. For example:
+
+// ["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
+// ["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
+
+// input: names - unsorted strings
+// output: case-agnostic sort
+const sortme = (arr) => {
+    return arr.sort((a,b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+}
+
+console.log(sortme(["Hello", "there", "I'm", "fine"] )) // ["fine", "Hello", "I'm", "there"]
+console.log(sortme(["C", "d", "a", "B"])) // ["a", "B", "C", "d"]
