6e1673805b666820e02cd9c5a8e5d7ab8d2a792a
Adam Robinson <adamrobinsonse@gmail.com>
2022-12-17 13:19:14 -0500

    added solution001.js

---
+++ 2022/12.17.22/solution001.js
@@ -0,0 +1,22 @@
+// The function takes in 2 inputs x and y, and should return x to the power of y
+
+// Simple, right? But you're NOT allowed to use Math.pow();
+
+// Obs: x and y will be naturals, so DON'T take fractions into consideration;
+
+// Note : zero to the power of zero equals one in this kata
+
+// Great coding <3
+
+function power(x,y){
+    let power = 1
+    for(let i = 1 ; i <= y ; i++) {  
+        power = power * x
+    }
+    return power
+}
+
+console.log(power(2,2)) // 4
+console.log(power(-1, 40)) // 1
+console.log(power(3, 2)) // 9
+console.log(power(3, 3)) // 27
